aa9a43b8c819ed37ff231038a345cc6b6df728ed
Hubert Karwowski <hubert.karwowski01@gmail.com>
2022-03-04 23:54:45 +0100

    added: sliders to a carousel with styles

---
+++ components/Slide.tsx
@@ -0,0 +1,36 @@
+export const Slide: React.FC = () => {
+  return (
+    <div className="relative flex h-screen w-screen flex-col justify-between overflow-x-hidden bg-green text-white lg:flex-row lg:items-center xl:px-24">
+      <div className="flex flex-col gap-4 p-8 md:gap-12 md:p-24 md:py-4 lg:w-2/4 ">
+        <div className="">
+          <img src="download.svg" className="w-xs" />
+        </div>
+        <h2 className="text-lg font-bold md:text-5xl">Pobierz aplikację</h2>
+        <p className="md:text-xl">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu
+          turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec
+          fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus
+          elit sed risus.
+        </p>
+        <div>
+          <button className="rounded-sm bg-blue py-2 px-3">
+            Pobierz aplikację
+          </button>
+        </div>
+      </div>
+      <div className="absolute bottom-0 top-32 m-auto flex w-full place-content-between px-5 lg:px-4 xl:left-0 xl:px-24">
+        <img src="arrow.svg" className="w-xs3 cursor-pointer md:w-xs2" />
+        <img
+          src="arrow.svg"
+          className="w-xs3 rotate-180 cursor-pointer bg-sm md:w-xs2"
+        />
+      </div>
+      <div className="bg-green lg:relative lg:w-2/4">
+        <img
+          src="app.svg"
+          className="m-auto w-sm md:w-lg lg:absolute lg:top-80 lg:bottom-0 lg:left-0 lg:right-0 lg:mx-auto"
+        />
+      </div>
+    </div>
+  )
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,26 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        green: 'rgba(38, 194, 153, 1)',
+        blue: 'rgba(10, 64, 161, 1)',
+        rose: 'rgba(250, 191, 186, 1)',
+      },
+      backgroundSize: {
+        sm: '2rem',
+        md: '4rem',
+      },
+      spacing: {
+        xl: '650px',
+        lg: '550px',
+        sm: '340px',
+        5: '5%',
+        xs: '60px',
+        xs2: '1.4rem',
+        xs3: '1rem',
+      },
+    },
   },
   plugins: [],
 }
